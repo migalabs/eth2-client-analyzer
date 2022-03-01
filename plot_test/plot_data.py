311d5dc98f492e72a84b99516a7b0549367f52b4
Tarun <tarsuno@gmail.com>
2022-03-01 18:34:56 +0100

    First version of new plotter

--- plot_test/plot_data.py
+++ plot_test/plot_data.py
@@ -49,10 +49,9 @@ It receives two areguments:
 
 This script not only outputs through terminal the results but also writes to disk in CSV format
 
+python3 plot_data.py disk 30 1000 "01/02/2022 00:00" file1.csv file2.csv...
 
 """
-
-
 
 
 from os import spawnl, sysconf
@@ -62,47 +61,12 @@ import datetime
 import sys
 
 import numpy as np
+import pandas as pd
 import os
+import matplotlib.dates as mdates
 
 
-
-
-# this function will calculate the time difference to the first timestamp.
-# if the difference with the last timestamp is less than 1 second, this new timestamp is ignored
-# if there is more than 1 second difference, we calculate the difference to the first timestamp
-# and append a new time to the array to be shown in the plot
-
-def add_time_to_array(input_array, last_datetime_str, new_datetime_str):
-
-    first_datetime = datetime.datetime.strptime(replace_month_name(input_array[0]), "%b %d %H:%M:%S:%f")
-    last_datetime = datetime.datetime.strptime(replace_month_name(last_datetime_str), "%b %d %H:%M:%S:%f")
-    new_datetime = datetime.datetime.strptime(replace_month_name(new_datetime_str), "%b %d %H:%M:%S:%f")
-
-
-
-    time_difference = new_datetime - first_datetime
-    
-    last_time_difference = new_datetime - last_datetime
-
-    if last_time_difference.seconds >= 1:
-
-        seconds = int(time_difference.seconds % 60)
-        rest = int(time_difference.seconds / 60)
-
-        minutes = int(rest % 60)
-        rest = int(rest / 60)
-
-        hours = int(rest % 60)
-
-        time_string = ""
-
-       
-        time_string = str(hours).zfill(2) + ":" + str(minutes).zfill(2) + ":" + str(seconds).zfill(2)
-
-        input_array.append(time_string)
-
-    return input_array
-
+initial_timestamp = datetime.datetime.strptime(sys.argv[4], '%d/%m/%Y %H:%M')
 
 def replace_month_name(input_str):
     result = input_str.replace("January", "Jan")
@@ -120,50 +84,36 @@ def replace_month_name(input_str):
     
     return result
 
-
-
 class ClientData():
 
-    def __init__(self, input_pid, input_name):
-        
-        self.pid = input_pid
-        self.name = input_name
+    def __init__(self, i_pid, i_name):
+        self.pid = i_pid
+        self.name = i_name
+
+        self.timestamp = []
         self.diskUsage = []
         self.cpuUsage = []
         self.memUsage = []
-
-
-        
-    def append_diskUsage_value(self, input_value):
-        float_value = float(input_value)
-
-        self.diskUsage.append(float("{:.2f}".format(float_value / 1000)))
-        
+        self.netSent = []
+        self.netReceived = []
+        self.currentSlot = []
+        self.currentPeers = []
     
-    def append_cpuUsage_value(self, input_value):
-
-        float_value = float(input_value)
-        self.cpuUsage.append(float("{:.2f}".format(float_value)))
+    def add_row(self, i_row):
+        self.timestamp.append(datetime.datetime.strptime(i_row[2], '%d/%m/%Y-%H:%M:%S:%f'))
+        self.diskUsage.append(float("{:.2f}".format(float(i_row[3]) / 1000)))
+        self.cpuUsage.append(float("{:.2f}".format(float(i_row[4]))))
+        self.memUsage.append(float("{:.2f}".format(float(i_row[5]))))
+        self.netSent.append(float("{:.2f}".format(float(i_row[6]))))
+        self.netReceived.append(float("{:.2f}".format(float(i_row[7]))))
+        self.currentSlot.append(int(i_row[8]))
+        self.currentPeers.append(int(i_row[9]))
     
-
-    def append_memUsage_value(self, input_value):
-
-        float_value = float(input_value)
-        self.memUsage.append(float("{:.2f}".format(float_value)))
+# checks if the specified name exists in an array of ClientData objects
+# returns the index of the specified client name in case of found.
+# Otherwise, return -1
     
-    # this method truncates data arrays to the specified length and removes the extra positions
-    def truncate_data_arrays(self, input_len):
-        
-        self.diskUsage = self.diskUsage[1:len]
-        self.cpuUsage = self.cpuUsage[1:len]
-        self.memUsage = self.memUsage[1:len]
-    
-
-    # checks if the specified name exists in an array of ClientData objects
-    # returns the index of the specified client name in case of found.
-    # Otherwise, return -1
-    @classmethod
-    def check_clientName_exists(cls, input_str, input_client_array):
+def check_clientName_exists(input_str, input_client_array):
 
         for idx, client in enumerate(input_client_array):
             if client.name == input_str:
@@ -172,37 +122,9 @@ class ClientData():
         return -1
 
 
-def main():
-    x = []
-    y = []
-
-    metricInterval = 0
-
-
-    metricName =  str(sys.argv[1]) 
-    metricType = 0 # default
-    # 0 for DiskUsage, 1 for CPUUsage, 2 for MemUSage
-    
-    if metricName == 'disk':
-        metricType = 1
-    elif metricName == 'cpu':
-        metricType = 2
-    elif metricName == 'mem':
-        metricType = 3
-    else:
-        print("Unknown metric type. Default (disk) applied.")
-        metricType = 1
-    
-    
-    # adjust path of file
-    data_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), str(sys.argv[2]))
-
-    client_object_array = []
-    x_array_time = []
-    last_datetime_str = ""
-
+def import_from_file(i_file, client_object_array):
     # open the file containing the data
-    with open(data_file,'r') as csvfile:
+    with open(i_file,'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         row_counter = 0
         
@@ -212,17 +134,16 @@ def main():
             if row_counter == 1:
                 # this is the title row
                 continue
-
-            if row_counter == 2:
-                # first data row
-                x_array_time.append(row[2])
-                last_datetime_str = x_array_time[-1]
+            
+            #print(row)
+            #if datetime.datetime.strptime(row[2], '%d/%m/%Y %H:%M:%S:%f') < initial_timestamp:
+            #    continue
                 
             client_pid = row[0]
             client_name = row[1]
             
             # get the position in our array to identify the client
-            client_index = ClientData.check_clientName_exists(client_name, client_object_array)
+            client_index = check_clientName_exists(client_name, client_object_array)
             
             if client_index == -1:
                 # this means we need to add the client object to the array
@@ -231,71 +152,168 @@ def main():
             
             # there is already an object for the specified client where to add the data
 
-            client_object_array[client_index].append_diskUsage_value(row[3])
-            client_object_array[client_index].append_cpuUsage_value(row[4])
-            client_object_array[client_index].append_memUsage_value(row[5])
+            client_object_array[client_index].add_row(row)
+
+
+"""
+Return the indexes of the timestamp_array_2 that are closer to each of the timestamp_array_1 items.
+We assume these arrays are ordered
+"""
+def find_nearest_spots(timestamp_array_1, timestamp_array_2):
+    array_2_index = 0
+    new_delta = abs((timestamp_array_1[0] - timestamp_array_2[array_2_index]).total_seconds())
+    result = []
+    for tmp_timestamp in timestamp_array_1:
+        delta = abs((tmp_timestamp - timestamp_array_2[array_2_index]).total_seconds())
+        new_delta = delta
+
+        while new_delta <= delta and array_2_index < len(timestamp_array_2)-1: # iterate until we find a greater delta
+            array_2_index = array_2_index + 1
+            delta = new_delta
+            new_delta = abs((tmp_timestamp - timestamp_array_2[array_2_index]).total_seconds())
+            #print("New delta is: " + str(new_delta))
+            #print("Old delta is: " + str(delta))
             
                 
-            x_array_time = add_time_to_array(x_array_time, last_datetime_str, row[2])
+        #print("Index in: " + str(array_2_index))
+        result.append(array_2_index-1)
+    #print(result)
 
-            last_datetime_str = row[2]
+    return result
+
+
+def main():
+    x = []
+    y = []
+
+    metricName =  str(sys.argv[1]) 
+    metricType = 1 # default
+
+    # 0 for DiskUsage, 1 for CPUUsage, 2 for MemUSage
+    yLabel = ""
+    graphTitle = ""
+
+    if metricName == 'disk':
+        metricType = 1
+        graphTitle = "Disk_Usage"
+        yLabel = "Disk Usage in GB"
+    elif metricName == 'cpu':
+        metricType = 2
+        graphTitle = "CPU_Usage"
+        yLabel = "CPU %"
+    elif metricName == 'mem':
+        metricType = 3
+        graphTitle = "MEM_Usage"
+        yLabel = "Memory [MB]"
+    else:
+        print("Unknown metric type. Default (disk) applied.")
+        metricType = 1
+    
+    num_of_ticks = int(sys.argv[2]) # number of ticks in the plot
+    number_of_lines = int(sys.argv[3]) # amount of data to plot
+    number_of_files = len(sys.argv) - 4 # first is the metric type and second is the interval, third is the amount of lines to process and fourth is the initial timestamp
+    data_files = []
+    for i in range(1, number_of_files, 1):
         
-        
+        data_files.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), str(sys.argv[i+4])))
+        print("Importing " + data_files[-1] + "...")
 
-    # check if data (y) and time (x) arrays have the same length
-    if len(x_array_time) > len(client_object_array[0].cpuUsage):
-        pass
+    client_object_array = []
 
-
+    for file in data_files:
+        import_from_file(file, client_object_array)
 
     yLabel = ""
     graphTitle = ""
 
-    plt.xlabel("Measure time (HH:MM:SS) since: " + x_array_time[0], fontsize=12)
-    x_array_time[0] = "00:00:00"
+    plt.xlabel("Measure time (HH:MM:SS) since: " + str(initial_timestamp), fontsize=12)
     print("You chose metric: ", metricType)
 
-    # obtain the client with less data
-    # we can use any of the arrays inside a ClientData object, as all of them get data
-    # appended and the same time. They should have the same length
-    minimumDataLength = min([len(x.diskUsage) for x in client_object_array])
-    timesTamp_length = len(x_array_time)
+    # calculate the maximum first date
+    minimumDate = max([x.timestamp[0] for x in client_object_array])
 
-    min_x_data = min(minimumDataLength, timesTamp_length)
+    # calculate the minimum last date
+    maximumDate = min([x.timestamp[-1] for x in client_object_array])
+    time_array = []
+
+    timedelta = maximumDate - minimumDate
+    stepSize = timedelta / num_of_ticks # step Size / jump size for plotting
+
+    tmp_timestamp = minimumDate
+
+    while tmp_timestamp < maximumDate:
+        time_array.append(tmp_timestamp)
+        tmp_timestamp = tmp_timestamp + stepSize
 
 
-    i = 1
-    # atempting to get 20 indexes in x axis
-    data_jump = int(min_x_data / 40)
-
-    # plot for each client
+    plot_data = []
     for client in client_object_array:
-
-        if metricType == 1:
-            plt.plot(x_array_time[0:min_x_data:data_jump], client.diskUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "Disk_Usage"
-            yLabel = "Disk Usage in GB"
-        
-        elif metricType == 2:
-            
-            plt.plot(x_array_time[0:min_x_data:data_jump], client.cpuUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "CPU_Usage"
-            yLabel = "CPU %"
-        
-        elif metricType == 3:
-
-            plt.plot(x_array_time[0:min_x_data:data_jump], client.memUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "MEM_Usage"
-            yLabel = "Memory [MB]"
-
-
-
-    plt.xticks(range(len(x_array_time[0:min_x_data:data_jump])), x_array_time[0:min_x_data:data_jump], rotation=45, ha="right")
-
+        data_indices = find_nearest_spots(time_array, client.timestamp)
+        plot_data.append([])
+        for item in data_indices:
+            #print(item)
+            if metricType == 1:
+                plot_data[-1].append(client.diskUsage[item])
+            if metricType == 2:
+                plot_data[-1].append(client.cpuUsage[item])
+            if metricType == 3:
+                plot_data[-1].append(client.memUsage[item])
+    
+        plt.plot(time_array, plot_data[-1], label=client.name)
+        #print(plot_data[-1])
+        #print(time_array)
+    #plt.xticks(range(len(time_array)), time_array, rotation=45, ha="right")
     plt.ylabel(yLabel)
     plt.title(graphTitle)
     plt.legend()
     plt.show()
 
+
+
+"""
+    i = 1
+    # atempting to get 40 indexes in x axis
+    data_jump = int(min_x_data / 40)
+
+
+
+    x_array_time.append(str(initial_timestamp))
+    #for i in range(1,number_of_lines):
+    #    last_time = x_array_time[i-1]
+    #    #print(last_time)
+    #    x_array_time.append(str((datetime.datetime.strptime(last_time, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(seconds=metricInterval))))
+
+    fig, axs = plt.subplots(1, 1, figsize=(6.4, 7), constrained_layout=True)
+    # plot for each client
+    for client in client_object_array:
+
+        if metricType == 1:
+            axs.plot(client.timestamp[0:min_x_data:data_jump], client.diskUsage[0:min_x_data:data_jump], label=client.name)
+            graphTitle = "Disk_Usage"
+            yLabel = "Disk Usage in GB"
+        
+        elif metricType == 2:
+            
+            axs.plot(client.timestamp[0:min_x_data:data_jump], client.cpuUsage[0:min_x_data:data_jump], label=client.name)
+            graphTitle = "CPU_Usage"
+            yLabel = "CPU %"
+        
+        elif metricType == 3:
+
+            axs.plot(client.timestamp[0:min_x_data:data_jump], client.memUsage[0:min_x_data:data_jump], label=client.name)
+            graphTitle = "MEM_Usage"
+            yLabel = "Memory [MB]"
+
+
+    axs.xaxis_date()
+    axs.grid(True)
+    axs.set_ylabel(yLabel)
+    plt.xticks(range(len(client.timestamp[0:min_x_data:data_jump])), client.timestamp[0:min_x_data:data_jump], rotation=45, ha="right")
+
+    #plt.ylabel(yLabel)
+    plt.title(graphTitle)
+    plt.legend()
+    plt.show()
+"""
 if __name__ == "__main__":
     main()
