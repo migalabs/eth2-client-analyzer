ce3cf7417307bd7d64d4f39ab1d16526f4b973f2
Tarun <tarsuno@gmail.com>
2022-03-03 09:32:04 +0100

    Added seq and slot mode

--- plot_test/plot_data.py
+++ plot_test/plot_data.py
@@ -54,7 +54,7 @@ python3 plot_data.py disk 30 1000 "01/02/2022 00:00" file1.csv file2.csv...
 """
 
 
-from os import spawnl, sysconf
+import configparser
 import matplotlib.pyplot as plt
 import csv
 import datetime
@@ -64,9 +64,21 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.dates as mdates
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
 
 
-initial_timestamp = datetime.datetime.strptime(sys.argv[4], '%d/%m/%Y %H:%M')
+colorMap = {
+    'prysm': '#FF6666',
+    'lighthouse': '#489dec',
+    'teku': 'orange',
+    'nimbus': 'green',
+    'lodestar': '#9379d6',
+    'grandine': 'yellow'
+}
+
+
+initial_timestamp = ""
 
 def replace_month_name(input_str):
     result = input_str.replace("January", "Jan")
@@ -100,11 +112,15 @@ class ClientData():
         self.currentPeers = []
     
     def add_row(self, i_row):
-        self.timestamp.append(datetime.datetime.strptime(i_row[2], '%d/%m/%Y-%H:%M:%S:%f'))
+        #self.timestamp.append(mktime(datetime.datetime.strptime(i_row[2], '%B %d %H:%M:%S:%f').timetuple()))
+        
+        self.timestamp.append(datetime.datetime.strptime(i_row[2], '%d/%m/%Y-%H:%M:%S:%f').timestamp())
+        #print(self.timestamp[-1])
         self.diskUsage.append(float("{:.2f}".format(float(i_row[3]) / 1000)))
         self.cpuUsage.append(float("{:.2f}".format(float(i_row[4]))))
         self.memUsage.append(float("{:.2f}".format(float(i_row[5]))))
-        self.netSent.append(float("{:.2f}".format(float(i_row[6]))))
+        #self.netSent.append(float("{:.2f}".format(float(i_row[6]))))
+        self.netSent.append(float(i_row[6]))
         self.netReceived.append(float("{:.2f}".format(float(i_row[7]))))
         self.currentSlot.append(int(i_row[8]))
         self.currentPeers.append(int(i_row[9]))
@@ -134,10 +150,6 @@ def import_from_file(i_file, client_object_array):
             if row_counter == 1:
                 # this is the title row
                 continue
-            
-            #print(row)
-            #if datetime.datetime.strptime(row[2], '%d/%m/%Y %H:%M:%S:%f') < initial_timestamp:
-            #    continue
                 
             client_pid = row[0]
             client_name = row[1]
@@ -158,64 +170,86 @@ def import_from_file(i_file, client_object_array):
 """
 Return the indexes of the timestamp_array_2 that are closer to each of the timestamp_array_1 items.
 We assume these arrays are ordered
+
 """
 def find_nearest_spots(timestamp_array_1, timestamp_array_2):
+
+
     array_2_index = 0
-    new_delta = abs((timestamp_array_1[0] - timestamp_array_2[array_2_index]).total_seconds())
+    new_delta = abs(timestamp_array_1[0] - timestamp_array_2[array_2_index])
     result = []
     for tmp_timestamp in timestamp_array_1:
-        delta = abs((tmp_timestamp - timestamp_array_2[array_2_index]).total_seconds())
+        delta = abs(tmp_timestamp - timestamp_array_2[array_2_index])
         new_delta = delta
 
         while new_delta <= delta and array_2_index < len(timestamp_array_2)-1: # iterate until we find a greater delta
             array_2_index = array_2_index + 1
             delta = new_delta
-            new_delta = abs((tmp_timestamp - timestamp_array_2[array_2_index]).total_seconds())
-            #print("New delta is: " + str(new_delta))
-            #print("Old delta is: " + str(delta))
-            
-                
-        #print("Index in: " + str(array_2_index))
+            new_delta = abs(tmp_timestamp - timestamp_array_2[array_2_index])
+        
         result.append(array_2_index-1)
-    #print(result)
 
+    #print(result)
     return result
 
 
 def main():
-    x = []
-    y = []
 
-    metricName =  str(sys.argv[1]) 
-    metricType = 1 # default
+    # prepare to parse config file
+    config_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), str(sys.argv[1]))
+    config_obj = configparser.ConfigParser()
+    
+
+    # check if file exists and read
+    if os.path.isfile(config_file):
+
+        config_obj.read(config_file)
+
+    else:
+        print("Config file not found")
+        exit()
+    
+    metricName = str(config_obj.get("BASIC", "METRIC_NAME"))
+    print("Metric name: " + str(metricName))
+
+    num_of_ticks = int(config_obj.get("BASIC", "NUM_OF_POINTS"))
+    print("Number of points: " + str(num_of_ticks))
+
+    initial_timestamp = datetime.datetime.strptime(config_obj.get("BASIC", "INITIAL_DATE"), '%d/%m/%Y %H:%M:%S').timestamp()
+    print("Initial timestamp: " + str(num_of_ticks))
+
+    xaxis_mode = str(config_obj.get("BASIC", "XAXIS"))
+    print("xaxis mode is: " + str(xaxis_mode))
+
+    secs_interval = int(config_obj.get("BASIC", "INTERVAL_SECS"))
+    print("Interval secs: " + str(secs_interval))
+
+    start_slot = int(config_obj.get("BASIC", "START_SLOT"))
+    print("Start slot: " + str(start_slot))
 
     # 0 for DiskUsage, 1 for CPUUsage, 2 for MemUSage
     yLabel = ""
     graphTitle = ""
-
+    print(metricName)
     if metricName == 'disk':
-        metricType = 1
+        
         graphTitle = "Disk_Usage"
         yLabel = "Disk Usage in GB"
     elif metricName == 'cpu':
-        metricType = 2
         graphTitle = "CPU_Usage"
         yLabel = "CPU %"
     elif metricName == 'mem':
-        metricType = 3
         graphTitle = "MEM_Usage"
         yLabel = "Memory [MB]"
     else:
         print("Unknown metric type. Default (disk) applied.")
-        metricType = 1
-    
-    num_of_ticks = int(sys.argv[2]) # number of ticks in the plot
-    number_of_lines = int(sys.argv[3]) # amount of data to plot
-    number_of_files = len(sys.argv) - 4 # first is the metric type and second is the interval, third is the amount of lines to process and fourth is the initial timestamp
+        metricName = 'disk'
+
+    number_of_args = len(sys.argv)
     data_files = []
-    for i in range(1, number_of_files, 1):
+    for i in range(2, number_of_args, 1):
         
-        data_files.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), str(sys.argv[i+4])))
+        data_files.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), str(sys.argv[i])))
         print("Importing " + data_files[-1] + "...")
 
     client_object_array = []
@@ -223,97 +257,114 @@ def main():
     for file in data_files:
         import_from_file(file, client_object_array)
 
-    yLabel = ""
-    graphTitle = ""
-
-    plt.xlabel("Measure time (HH:MM:SS) since: " + str(initial_timestamp), fontsize=12)
-    print("You chose metric: ", metricType)
-
-    # calculate the maximum first date
-    minimumDate = max([x.timestamp[0] for x in client_object_array])
-
-    # calculate the minimum last date
-    maximumDate = min([x.timestamp[-1] for x in client_object_array])
-    time_array = []
-
-    timedelta = maximumDate - minimumDate
-    stepSize = timedelta / num_of_ticks # step Size / jump size for plotting
-
-    tmp_timestamp = minimumDate
-
-    while tmp_timestamp < maximumDate:
-        time_array.append(tmp_timestamp)
-        tmp_timestamp = tmp_timestamp + stepSize
-
-
-    plot_data = []
-    for client in client_object_array:
-        data_indices = find_nearest_spots(time_array, client.timestamp)
-        plot_data.append([])
-        for item in data_indices:
-            #print(item)
-            if metricType == 1:
-                plot_data[-1].append(client.diskUsage[item])
-            if metricType == 2:
-                plot_data[-1].append(client.cpuUsage[item])
-            if metricType == 3:
-                plot_data[-1].append(client.memUsage[item])
     
-        plt.plot(time_array, plot_data[-1], label=client.name)
-        #print(plot_data[-1])
-        #print(time_array)
-    #plt.xticks(range(len(time_array)), time_array, rotation=45, ha="right")
+    print("You chose metric: ", metricName)
+
+    x_array = []
+    x_labels = []
+
+    fig, ax = plt.subplots()
+    plot_data = []
+
+    if xaxis_mode == 'date':
+        # calculate the maximum first date
+        minimumDate = initial_timestamp
+
+        # calculate the minimum last date
+        maximumDate = min([x.timestamp[-1] for x in client_object_array])
+
+        timedelta = maximumDate - minimumDate
+        stepSize = timedelta / num_of_ticks # step Size / jump size for plotting
+
+        tmp_timestamp = minimumDate
+
+        while tmp_timestamp < maximumDate:
+            x_array.append(tmp_timestamp)
+            tmp_timestamp = tmp_timestamp + stepSize
+            #x_labels.append(x_array[-1])
+            x_labels.append(datetime.datetime.utcfromtimestamp(tmp_timestamp))
+        
+        for client in client_object_array:
+            data_indices = find_nearest_spots(x_array, client.timestamp)
+            plot_data.append([])
+            for item in data_indices:
+                if metricName == 'disk':
+                    plot_data[-1].append(client.diskUsage[item])
+                if metricName == 'cpu':
+                    plot_data[-1].append(client.cpuUsage[item])
+                if metricName == 'mem':
+                    plot_data[-1].append(client.memUsage[item])
+        
+            ax.plot(x_labels, plot_data[-1], label=client.name, color=colorMap[client.name])
+        
+        majorTicksFormat = mdates.DateFormatter('%d/%m/%Y-%H:%M:%S')
+        ax.xaxis.set_major_formatter(majorTicksFormat)
+        fig.autofmt_xdate(rotation=45)
+        
+
+    if xaxis_mode == 'seq':
+        # calculate the maximum first date
+        #minimumDate = max([x.timestamp[0] for x in client_object_array])
+        maxLength = min([len(x.timestamp) for x in client_object_array])
+
+        stepSize = int(maxLength / num_of_ticks) # step Size / jump size for plotting
+
+        data_indices = range(0, maxLength, stepSize)
+        x_labels = [secs_interval*x/3600 for x in data_indices]
+        
+        for client in client_object_array:
+            
+            plot_data.append([])
+            for item in data_indices:
+                if metricName == 'disk':
+                    plot_data[-1].append(client.diskUsage[item])
+                if metricName == 'cpu':
+                    plot_data[-1].append(client.cpuUsage[item])
+                if metricName == 'mem':
+                    plot_data[-1].append(client.memUsage[item])
+        
+            ax.plot(x_labels, plot_data[-1], label=client.name, color=colorMap[client.name])
+        
+        plt.xlabel("Hours", fontsize=12)
+    
+    if xaxis_mode == 'slot':
+
+        # calculate the minimum last date
+        maximumSlot = min([x.currentSlot[-1] for x in client_object_array])
+
+        stepSize = int((maximumSlot-start_slot) / num_of_ticks) # step Size / jump size for plotting
+        x_labels = range(start_slot, maximumSlot, stepSize)
+        
+        for client in client_object_array:
+            data_indices = find_nearest_spots(x_labels, client.currentSlot)
+            print(data_indices)
+            plot_data.append([]) 
+            for item in data_indices:
+                if metricName == 'disk':
+                    plot_data[-1].append(client.diskUsage[item])
+                if metricName == 'cpu':
+                    plot_data[-1].append(client.cpuUsage[item])
+                if metricName == 'mem':
+                    plot_data[-1].append(client.memUsage[item])
+        
+            
+            ax.set_xticklabels(['{:,.0f}'.format(x) for x in x_labels])
+            ax.plot(x_labels, plot_data[-1], label=client.name, color=colorMap[client.name])
+
+        
+        plt.xlabel("Slot", fontsize=12)
+
+    print(graphTitle)
+    ax.grid(axis='y')
+    ax.grid(axis='x')
     plt.ylabel(yLabel)
     plt.title(graphTitle)
     plt.legend()
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    
+    #ax.tick_params(axis="x", which="both", rotation=45)
     plt.show()
 
 
-
-"""
-    i = 1
-    # atempting to get 40 indexes in x axis
-    data_jump = int(min_x_data / 40)
-
-
-
-    x_array_time.append(str(initial_timestamp))
-    #for i in range(1,number_of_lines):
-    #    last_time = x_array_time[i-1]
-    #    #print(last_time)
-    #    x_array_time.append(str((datetime.datetime.strptime(last_time, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(seconds=metricInterval))))
-
-    fig, axs = plt.subplots(1, 1, figsize=(6.4, 7), constrained_layout=True)
-    # plot for each client
-    for client in client_object_array:
-
-        if metricType == 1:
-            axs.plot(client.timestamp[0:min_x_data:data_jump], client.diskUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "Disk_Usage"
-            yLabel = "Disk Usage in GB"
-        
-        elif metricType == 2:
-            
-            axs.plot(client.timestamp[0:min_x_data:data_jump], client.cpuUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "CPU_Usage"
-            yLabel = "CPU %"
-        
-        elif metricType == 3:
-
-            axs.plot(client.timestamp[0:min_x_data:data_jump], client.memUsage[0:min_x_data:data_jump], label=client.name)
-            graphTitle = "MEM_Usage"
-            yLabel = "Memory [MB]"
-
-
-    axs.xaxis_date()
-    axs.grid(True)
-    axs.set_ylabel(yLabel)
-    plt.xticks(range(len(client.timestamp[0:min_x_data:data_jump])), client.timestamp[0:min_x_data:data_jump], rotation=45, ha="right")
-
-    #plt.ylabel(yLabel)
-    plt.title(graphTitle)
-    plt.legend()
-    plt.show()
-"""
 if __name__ == "__main__":
     main()
