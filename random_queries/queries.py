d64956cbf5556fc781c38156962b398b8a77f224
Tarun <tarsuno@gmail.com>
2022-03-17 10:04:15 +0100

    Added some prints and refresh config file

--- random_queries/queries.py
+++ random_queries/queries.py
@@ -19,12 +19,12 @@ TIMEOUT = 10
 def do_replace_requests(i_port, i_path, i_num_of_queries, i_name, i_rand_numbers):
     result = []
     # headers
-    result.append([])
-    result[-1].append("NAME")
-    result[-1].append("TIMESTAMP")
-    result[-1].append("PATH")
-    result[-1].append("MILISECONDS_REQ") # from response.elapsed.microseconds
-    result[-1].append("MILISECONDS_DELTA") # from measure timestamp difference
+    #result.append([])
+    #result[-1].append("NAME")
+    #result[-1].append("TIMESTAMP")
+    #result[-1].append("PATH")
+    #result[-1].append("MILISECONDS_REQ") # from response.elapsed.microseconds
+    #result[-1].append("MILISECONDS_DELTA") # from measure timestamp difference
 
     for i in range(0, i_num_of_queries, 1):
         result.append([])
@@ -41,21 +41,22 @@ def do_replace_requests(i_port, i_path, i_num_of_queries, i_name, i_rand_numbers
     for idx in range(0, i_num_of_queries, 1):
 
         first_timestamp = datetime.now()
-        response = requests.get(result[idx+1][2], timeout = TIMEOUT) # first line is the title
+        response = requests.get(result[idx][2], timeout = TIMEOUT) # first line is the title
         second_timestamp = datetime.now()
         delta = (second_timestamp - first_timestamp)
         delta = delta / timedelta(milliseconds=1)
-        result[idx+1].append(response.elapsed.microseconds/1000)
-        result[idx+1].append(delta)
+        result[idx].append(response.elapsed.microseconds/1000)
+        result[idx].append(delta)
         #print(idx)
 
     second_whole_timestamp = datetime.now()
     whole_delta = second_whole_timestamp - first_whole_timestamp # delta
     whole_delta = whole_delta / timedelta(milliseconds=1) # microseconds
     whole_delta = whole_delta / i_num_of_queries # average
-    print("Start time:  ", str(first_whole_timestamp))
-    print("Finish time: ", str(second_whole_timestamp))
-    print("Average Delta:    ", whole_delta, "  MILISECONDS")
+    print("Start time:                        ", str(first_whole_timestamp))
+    print("Finish time:                       ", str(second_whole_timestamp))
+    print("Average Delta (Finish - Start):    ", whole_delta, "  MILISECONDS")
+    
     return result
 
 
@@ -95,8 +96,11 @@ def main():
         i_name = str(config_obj.get(section_name, "NAME"))
         i_csv_path = str(config_obj.get(section_name, "CSV_FILE_PATH"))
 
+        print(i_name)
 
         result = np.array(do_replace_requests(i_port, i_path, num_of_queries, i_name, rand_numbers))
+        print("Average Req:                       ", result[:,3].astype(np.float64).mean(), "  MILISECONDS")
+        print("Average Row Delta:                 ", result[:,4].astype(np.float64).mean(), "  MILISECONDS")
         #print(result)
         np.savetxt(i_csv_path, result, delimiter=',', fmt='%s')
 
