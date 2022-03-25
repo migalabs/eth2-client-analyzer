87953f97ab2e653415cb12eb88a9773a50269012
Tarun <tarsuno@gmail.com>
2022-03-25 16:34:25 +0100

    Adde several request types

--- random_queries/queries.py
+++ random_queries/queries.py
@@ -14,15 +14,49 @@ from async_timeout import timeout
 import numpy as np
 
 LOCALHOST = "http://localhost:"
-TIMEOUT = 10
+MAX_VALIDATOR = 21063
+TIMEOUT = 20
+TAB_SIZE = 10
+
+class RandomPaths:
+
+    def __init__(self, i_config):
+        
+        config_obj = i_config
+        
+        self.num_of_queries = int(config_obj.get("BASIC", "NUM_QUERIES"))
+        self.max_rand = int(config_obj.get("BASIC", "MAX_RANDOM")) # generate random numbers between 0 and this
+        
+        self.result = []
+
+class BlockPath(RandomPaths):
+    pass
+
+class StatePath(RandomPaths):
+    def __init__(self, i_config):
+        super().__init__(i_config)
+        self.random_block_numbers = []
+        self.random_val_index = []
+    
+    def execute(self, i_port):
+        for _ in range(0, self.num_of_queries, 1):
+            random_block = np.random.randint(0, self.max_rand)
+            random_val = np.random.randint(0, MAX_VALIDATOR)
+            self.random_block_numbers.append(random_block)
+            self.random_val_index.append(random_val)
+
+
+            full_path = LOCALHOST + str(i_port) + "/eth/v1/beacon/states/" + str(random_block) + "/validator_balances?id=" + str(random_val)
+            self.result.append(full_path)
+
 
 # rand_numbers will be the same length as num_of_queries
 # this function basically performs as many requests as random numbers are given
-def do_random_requests(i_num_of_queries, i_name, i_rand_paths):
+def do_random_requests(i_name, i_rand_paths):
     result = []
 
     # fill the result matrix with basic data
-    for i in range(0, i_num_of_queries, 1):
+    for i in range(0, len(i_rand_paths), 1):
         result.append([])
         result[-1].append(i_name)
         result[-1].append(str(datetime.now()))
@@ -31,25 +65,34 @@ def do_random_requests(i_num_of_queries, i_name, i_rand_paths):
     # measure whole time
     first_whole_timestamp = datetime.now()
 
-    for idx in range(0, i_num_of_queries, 1):
-        
-        # each query measure time lapse separately as well
-        first_timestamp = datetime.now()
-        response = requests.get(result[idx][2], timeout = TIMEOUT) # in each line, position 2 is the url
-        second_timestamp = datetime.now()
-        delta = (second_timestamp - first_timestamp)
-        delta = delta / timedelta(milliseconds=1) #individual time lapse
-        result[idx].append(response.elapsed.microseconds/1000) # time lapse python package provides
-        result[idx].append(delta)
+    for idx in range(0, len(i_rand_paths), 1):
+        try:
+            # each query measure time lapse separately as well
+            first_timestamp = datetime.now()
+            response = requests.get(result[idx][2], timeout = TIMEOUT) # in each line, position 2 is the url
+            second_timestamp = datetime.now()
+            delta = (second_timestamp - first_timestamp)
+            delta = delta / timedelta(milliseconds=1) #individual time lapse
+            result[idx].append(response.elapsed.microseconds/1000) # time lapse python package provides
+            result[idx].append(delta)
+            result[idx].append(int(response.status_code))
+
+            print(delta)
+            print(response.elapsed.microseconds/1000)
+            #print(response.text)
+        except: # probably a timeout
+            result[idx].append(TIMEOUT * 1000) # time lapse python package provides
+            result[idx].append(TIMEOUT * 1000)
+            result[idx].append(408)
 
     # calculate full time lapse and avergae
     second_whole_timestamp = datetime.now()
     whole_delta = second_whole_timestamp - first_whole_timestamp # delta
     whole_delta = whole_delta / timedelta(milliseconds=1) # microseconds
-    whole_delta = whole_delta / i_num_of_queries # average
+    whole_delta = whole_delta / len(i_rand_paths) # average
     print("Start time:                        ", str(first_whole_timestamp))
     print("Finish time:                       ", str(second_whole_timestamp))
-    print("Average Delta (Finish - Start):    ", whole_delta, "  MILISECONDS")
+    print("Average Delta (Finish - Start):    ", f"{whole_delta:<10}",  "MILISECONDS")
     
     return result
 
@@ -76,53 +119,55 @@ def main():
 
     num_of_queries = int(config_obj.get("BASIC", "NUM_QUERIES"))
     max_rand = int(config_obj.get("BASIC", "MAX_RANDOM")) # generate random numbers between 0 and this
-
-    rand_numbers = []
     rand_paths = []
-    
-    # read file containing ranom paths in case it exists
-    if config_obj.get("BASIC", "RANDOM_PATHS_FILE") != "":
-        random_paths_file = str(config_obj.get("BASIC", "RANDOM_PATHS_FILE"))
-        with open(random_paths_file, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            rand_paths = [line[0] for line in reader]
-
-    for _ in range(0, num_of_queries, 1):
-        rand_numbers.append(np.random.randint(0, max_rand))
 
     while config_obj.has_section(section_name): # for each section of type SETx
 
+        
+
+
         i_port = int(config_obj.get(section_name, "PORT"))
-        i_path = str(config_obj.get(section_name, "PATH"))
+        i_mode = str(config_obj.get(section_name, "MODE"))
         i_name = str(config_obj.get(section_name, "NAME"))
         i_csv_path = str(config_obj.get(section_name, "CSV_FILE_PATH"))
+
+
+        # read file containing ranom paths in case it exists
+        if config_obj.get(section_name, "RANDOM_PATHS_FILE") != "":
+            random_paths_file = str(config_obj.get(section_name, "RANDOM_PATHS_FILE"))
+            with open(random_paths_file, newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                rand_paths = [line[0] for line in reader]
+        else:
+            random_obj = StatePath(config_obj)
+            random_obj.execute(i_port)
+            rand_paths = random_obj.result
 
         print(i_name)
         print("Number of queries:", num_of_queries)
 
-        # in case a file containing paths to query, do not generate array of random numbers
-        if len(rand_paths) == 0:
-            # we did not receive any path file
-            base_path = str(LOCALHOST) + str(i_port) + str(i_path)
-            rand_paths = [base_path.replace("/xyz", "/"+str(x)) for x in rand_numbers] # generate the rand urls
-
-
-        result = np.array(do_random_requests(num_of_queries, i_name, rand_paths))
+        result = np.array(do_random_requests(i_name, rand_paths))
 
         #Calculate stats
         min_value_index = result[:,3].astype(np.float64).argmin()
         max_value_index = result[:,3].astype(np.float64).argmax()
-        print("Average Req:                       ", result[:,3].astype(np.float64).mean(), "  MILISECONDS")
-        print("Average Row Delta:                 ", result[:,4].astype(np.float64).mean(), "  MILISECONDS")
-        print("Best score:                        ", result[min_value_index][3],   "  MILISECONDS", result[min_value_index][2])
-        print("Worst score:                       ", result[max_value_index][3],   "  MILISECONDS", result[max_value_index][2])
+        req_mean = "{:.2f}".format(result[:,3].astype(np.float64).mean())
+        row_delta_mean = "{:.2f}".format(result[:,4].astype(np.float64).mean())
+        best_score = "{:.2f}".format(result[min_value_index][3].astype(np.float64))
+        worst_score = "{:.2f}".format(result[max_value_index][3].astype(np.float64))
 
 
+        print("Average Req:                       ", f"{req_mean:<10}", "MILISECONDS")
+        print("Average Row Delta:                 ", f"{row_delta_mean:<10}", "MILISECONDS")
+        print("Best score:                        ", f"{best_score:<10}",   "MILISECONDS", result[min_value_index][2])
+        print("Worst score:                       ", f"{worst_score:<10}",   "MILISECONDS", result[max_value_index][2])
+        print("200 score:                         ", result[:,5].tolist().count("200") / num_of_queries * 100, "%")
+
+        result = np.insert(result, 0, np.array(["NAME", "TIMESTAMP", "PATH", "REQ_TIME", "DELTA_TIME", "RESPONSE_CODE"]), 0)
         np.savetxt(i_csv_path, result, delimiter=',', fmt='%s')
+        
 
-        # if there was no random paths file, we now save the generated one
-        if config_obj.get("BASIC", "RANDOM_PATHS_FILE") != "":
-            np.savetxt("./random.csv", rand_paths, delimiter=',', fmt='%s')
+        np.savetxt("./random1.csv", rand_paths, delimiter=',', fmt='%s')
 
         section_number = section_number + 1
         section_name = str(section_base_name + str(section_number))
